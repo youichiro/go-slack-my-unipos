f97564775b1d3d15d7c0c585d3189ae54a1f693f
youichiro <cinnamon416@gmail.com>
2022-10-01 13:40:16 +0900

    create postgres repository

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "github.com/youichiro/go-slack-my-unipos/pkg/router"
+import (
+	"github.com/youichiro/go-slack-my-unipos/internal/repositories"
+	"github.com/youichiro/go-slack-my-unipos/internal/router"
+)
 
 func main() {
+	dbRepo := &repositories.PostgresRepository{}
+	err := dbRepo.Connect()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer dbRepo.Close()
+
 	r := router.SetupRouter()
-	err := r.Run(":8080")
+	err = r.Run(":8080")
 	if err != nil {
 		panic(err.Error())
 	}
